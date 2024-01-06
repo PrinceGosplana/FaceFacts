1e38047161c0ad66736a811497ec4aaf627cced3
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-06 15:30:37 +0200

    init the project

---
+++ FaceFacts/FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by OLEKSANDR ISAIEV on 06.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
