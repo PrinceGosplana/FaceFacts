47a8f5c7059ea86732d8f09a9a84740cf8da551e
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-06 15:35:54 +0200

    create person model

---
+++ FaceFacts/FaceFacts/Person.swift
@@ -0,0 +1,22 @@
+//
+//  Person.swift
+//  FaceFacts
+//
+//  Created by OLEKSANDR ISAIEV on 06.01.2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Person {
+    var name: String
+    var emailAddress: String
+    var details: String
+    
+    init(name: String, emailAddress: String, details: String) {
+        self.name = name
+        self.emailAddress = emailAddress
+        self.details = details
+    }
+}
