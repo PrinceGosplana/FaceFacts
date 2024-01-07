9a84fd55adcd05bbd294c718c4c3ee00939ff20c
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-07 07:42:57 +0200

    query data where we need

--- FaceFacts/FaceFacts/ContentView.swift
+++ FaceFacts/FaceFacts/ContentView.swift
@@ -5,9 +5,12 @@
 //  Created by OLEKSANDR ISAIEV on 06.01.2024.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Query var people: [Person]
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
