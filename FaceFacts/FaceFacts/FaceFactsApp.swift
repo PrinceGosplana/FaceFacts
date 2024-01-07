57ca476eff1ef0211a256c6adaafd4322ef09cbe
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-07 07:39:35 +0200

    add model to container

--- FaceFacts/FaceFacts/FaceFactsApp.swift
+++ FaceFacts/FaceFacts/FaceFactsApp.swift
@@ -5,6 +5,7 @@
 //  Created by OLEKSANDR ISAIEV on 06.01.2024.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct FaceFactsApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Person.self)
     }
 }
