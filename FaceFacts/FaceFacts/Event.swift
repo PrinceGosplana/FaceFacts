6f1f1caf1023fbaa83ec1fbf709b5ba659f3b9d0
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-09 10:39:34 +0200

    add new event view

---
+++ FaceFacts/FaceFacts/Event.swift
@@ -0,0 +1,18 @@
+//
+//  Event.swift
+//  FaceFacts
+//
+//  Created by OLEKSANDR ISAIEV on 09.01.2024.
+//
+
+import SwiftUI
+
+struct Event: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Event()
+}
