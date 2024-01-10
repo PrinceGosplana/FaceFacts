dec95d4632da8a26b3a00086b2dc4ca1c1836a77
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-10 13:24:18 +0200

    extend event model and person model

--- FaceFacts/FaceFacts/Event.swift
+++ FaceFacts/FaceFacts/Event.swift
@@ -12,6 +12,7 @@ import SwiftData
 class Event {
     var name: String
     var location: String
+    var people = [Person]()
     
     init(name: String, location: String) {
         self.name = name
