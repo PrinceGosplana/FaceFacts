87fe2d270d31ccf42c21cfd5f27fd1c009815e02
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-11 05:57:43 +0200

    add view for editing event

---
+++ FaceFacts/FaceFacts/EditEventView.swift
@@ -0,0 +1,18 @@
+//
+//  EditEventView.swift
+//  FaceFacts
+//
+//  Created by OLEKSANDR ISAIEV on 11.01.2024.
+//
+
+import SwiftUI
+
+struct EditEventView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditEventView()
+}
